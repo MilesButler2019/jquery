cfc9ad53209040769a2dcbbd6cb9f1a1b1ac7f21
MilesButler2019 <47306315+MilesButler2019@users.noreply.github.com>
2019-12-05 00:20:17 -0500

    Add files via upload
    Updated mouseover function.

---
+++ challenge5.js
@@ -0,0 +1,20 @@
+$(function(){
+  $("img").mouseover(function(){
+      $("#image").css("background-image","url('"+$(this).attr("src")+"')");
+      $("#image").html($(this).attr("alt"));
+  })
+  $("img").mouseout(function(){
+    $("#image").css("background-image","");
+    $('#image').html("Hover over an image below.");
+  })
+  $("img").focus(function(){
+      $("#image").css("background-image","url('"+$(this).attr("src")+"')")
+      $('#image').html($(this).attr("alt"))
+  })
+  $("img").blur(function(){
+      $("#image").css("background-image","url('"+$(this).attr("src")+"')")
+      $('#image').html($(this).attr("alt"))
+  })
+
+
+})
